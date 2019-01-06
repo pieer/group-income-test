64cedbfba8f6f85d903a6c6cea9d22009834ed6a
Pierre Schweiger <pierre.schweiger@regily.com>
2019-01-06 17:52:54 -0300

    Fix error on safari for insecure access of the localstorage

--- src/store.js
+++ src/store.js
@@ -3,12 +3,21 @@ import Vuex from 'vuex'
 import Colors from './data/colors.json'
 
 Vue.use(Vuex)
+let fontSize = 62.5
+let color = 'blue'
+try {
+  fontSize = parseFloat(localStorage.getItem('fontSize'))
+  color = localStorage.getItem('theme')
+} catch(err) {
+  // eslint-disable-next-line
+  console.log(err)
+}
 
 const state = {
   modalVisible: false,
   modalComponent: null,
-  fontSize: parseFloat(localStorage.getItem('fontSize')) || 62.5,
-  colors: Colors[localStorage.getItem('theme') || 'blue']
+  fontSize: fontSize,
+  colors: Colors[color]
 }
 
 const mutations = {
@@ -26,7 +35,13 @@ const mutations = {
     state.fontSize = size
     if (typeof(Storage) !== 'undefined') {
       // Store the font size in local storage
-      localStorage.setItem('fontSize', size)
+      try {
+        localStorage.setItem('fontSize', size)
+      } catch (error) {
+        // eslint-disable-next-line
+        console.log(error)
+      }
+      
     }
   },
 
@@ -34,7 +49,12 @@ const mutations = {
     state.colors = colors
     if (typeof(Storage) !== 'undefined') {
       // Store the theme in local storage
-      localStorage.setItem('theme', colors.name)
+      try {
+        localStorage.setItem('theme', colors.name)
+      } catch (error) {
+        // eslint-disable-next-line
+        console.log(error)
+      }
     }
   }
 }
